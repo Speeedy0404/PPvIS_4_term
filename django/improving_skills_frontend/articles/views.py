c3f8346e3dc7eb3d0e60ff5abe7be240535f6626
Nikita Semchenkov <bachilo.g@mail.ru>
2022-05-01 17:42:44 +0300

    Some upgrate

--- django/improving_skills_frontend/articles/views.py
+++ django/improving_skills_frontend/articles/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse, HttpResponseNotFound, Http404
-from django.shortcuts import redirect, render, get_object_or_404
+from django.http import HttpResponse, HttpResponseNotFound
+from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
+
 from .forms import AddPostForm
 from .models import *
 
@@ -11,6 +13,22 @@ navigation = [
 ]
 
 
+class ArticlesHome(ListView):
+    model = Articles
+    template_name = 'articles/index.html'
+    context_object_name = 'posts'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
+
+    def get_queryset(self):
+        return Articles.objects.filter(is_published=True)
+
+
+"""
 def index(request):
     posts = Articles.objects.all()
     context = {
@@ -19,25 +37,37 @@ def index(request):
         'cat_selected': 0,
     }
     return render(request, 'articles/index.html', context=context)
+"""
 
 
 def about(request):
     return render(request, 'articles/about.html', {'title': 'О сайте'})
 
 
+class AddPage(CreateView):
+    form_class = AddPostForm
+    template_name = 'articles/addpage.html'
+
+    # success_url = reverse_lazy('home')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Добавление статьи'
+        return context
+
+
+"""
 def addpage(request):
     if request.method == 'POST':
-        form = AddPostForm(request.POST)
+        form = AddPostForm(request.POST, request.FILES)
         if form.is_valid():
-            try:
-                Articles.objects.create(**form.cleaned_data)
-                return redirect('home')
-            except:
-                form.add_error(None, 'Ошибка добавления')
+            form.save()
+            return redirect('home')
     else:
         form = AddPostForm()
 
     return render(request, 'articles/addpage.html', {'form': form, 'title': 'Добавление статьи'})
+"""
 
 
 def contact(request):
@@ -48,6 +78,20 @@ def login(request):
     return HttpResponse('login')
 
 
+class ShowPost(DetailView):
+    model = Articles
+    template_name = 'articles/post.html'
+    slug_url_kwarg = 'post_slug'
+    context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = context['post']
+        context['cat_selected'] = context['post'].cat_id
+        return context
+
+
+"""
 def show_post(request, post_slug):
     post = get_object_or_404(Articles, slug=post_slug)
     context = {
@@ -56,8 +100,26 @@ def show_post(request, post_slug):
         'cat_selected': post.cat_id,
     }
     return render(request, 'articles/post.html', context=context)
+"""
 
 
+class ArticlesCategory(ListView):
+    model = Articles
+    template_name = 'articles/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat_id
+        return context
+
+    def get_queryset(self):
+        return Articles.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+
+"""
 def show_category(request, cat_slug):
     cat = Category.objects.get(slug=cat_slug)
     posts = Articles.objects.filter(cat_id=cat.id)
@@ -71,6 +133,7 @@ def show_category(request, cat_slug):
         'cat_selected': cat.id,
     }
     return render(request, 'articles/index.html', context=context)
+"""
 
 
 def page_not_found(request, exception):
